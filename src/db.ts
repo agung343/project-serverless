459c48c30749602573779d8e911a0a9b7ff2d6e0
agung343 <agungwahydi343@gmail.com>
2026-03-25 11:16:45 +0700

    add-unit-not-tested

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,10 @@
 // import { drizzle } from "drizzle-orm/neon-http"
 // import { neon} from "@neondatabase/serverless"
 
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-serverless";
+import { drizzle as drizzlePg } from "drizzle-orm/node-postgres"
 import { Pool } from "@neondatabase/serverless";
+import { Pool as PoolPg} from "pg"
 
 import * as tenantSchema from "./db/schema/tenants"
 import * as userSchema from "./db/schema/users"
@@ -18,15 +20,21 @@ const schema = {
     ...productSchema,
 }
 
-
-// export const connectDB = (connectionString: string) => {
-//     const sql = neon(connectionString)
-//     return drizzle(sql, {schema})
-// }
+const isProd = process.env.NODE_ENV === "production"
 
 export const connectDB = (connectionString: string) => {
-    const pool = new Pool({connectionString})
-    return drizzle(pool, {schema})
+    if (isProd) {
+        const pool = new Pool({connectionString}) 
+        return drizzleNeon(pool, {schema})
+    } else {
+        const pool = new PoolPg({connectionString}) 
+        return drizzlePg(pool, {schema})
+    }
 }
+
+// export const connectDB = (connectionString: string) => {
+//     const pool = new Pool({connectionString})
+//     return drizzle(pool, {schema})
+// }
 
 export type DbClient = ReturnType<typeof connectDB>
--- src/db/schema/products.ts
+++ src/db/schema/products.ts
@@ -1,24 +1,48 @@
-import { pgTable, text, timestamp, unique, numeric, integer, check, index, foreignKey } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  timestamp,
+  unique,
+  numeric,
+  integer,
+  check,
+  index,
+  foreignKey,
+} from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
 import { relations, sql } from "drizzle-orm";
 import { categories } from "./categories";
 import { tenants } from "./tenants";
+import { units } from "./units";
 
-export const products = pgTable("products", {
+export const products = pgTable(
+  "products",
+  {
     id: text("id").primaryKey().$defaultFn(createId),
     name: text("name").notNull(),
     slug: text("slug").notNull(),
     code: text("code").notNull(),
     price: integer("price").notNull().default(0),
     cost: integer("cost").notNull().default(0),
-    stock: numeric("stock", {precision: 10, scale: 3}).notNull().default("0"),
+    stock: numeric("stock", { precision: 10, scale: 3 }).notNull().default("0"),
     sold: integer("sold").notNull().default(0),
     description: text("description"),
-    categoryId: text("category_id").notNull().references(() => categories.id),
-    tenantId: text("tenant_id").notNull().references(() => tenants.id, {onDelete: "cascade"}),
+    categoryId: text("category_id")
+      .notNull()
+      .references(() => categories.id),
+    unitId: integer("unit_id")
+      .notNull()
+      .references(() => units.id),
+    tenantId: text("tenant_id")
+      .notNull()
+      .references(() => tenants.id, { onDelete: "cascade" }),
     createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().$onUpdate(() => new Date()).notNull()
-}, (table) => [
+    updatedAt: timestamp("updated_at")
+      .defaultNow()
+      .$onUpdate(() => new Date())
+      .notNull(),
+  },
+  (table) => [
     unique().on(table.tenantId, table.code),
     unique().on(table.tenantId, table.slug),
     index("product_tenant_idx").on(table.tenantId),
@@ -29,22 +53,26 @@ export const products = pgTable("products", {
     check("stock_positive", sql`${table.stock} >= 0`),
 
     foreignKey({
-        columns: [table.categoryId, table.tenantId],
-        foreignColumns: [categories.id, categories.tenantId]
-    })
-])
-
-export const productsRelations = relations(products, ({one}) => ({
-    category: one(categories, {
-        fields: [products.categoryId],
-        references: [categories.id]
+      columns: [table.categoryId, table.tenantId],
+      foreignColumns: [categories.id, categories.tenantId],
     }),
-    tenant: one(tenants, {
-        fields: [products.tenantId],
-        references: [tenants.id]
-    })
-}))
+  ]
+);
 
+export const productsRelations = relations(products, ({ one }) => ({
+  category: one(categories, {
+    fields: [products.categoryId],
+    references: [categories.id],
+  }),
+  tenant: one(tenants, {
+    fields: [products.tenantId],
+    references: [tenants.id],
+  }),
+  unit: one(units, {
+    fields: [products.unitId],
+    references: [units.id], // Ensure proper reference to units.id
+  }),
+}));
 
 export type Product = typeof products.$inferSelect;
 export type NewProduct = typeof products.$inferInsert;
